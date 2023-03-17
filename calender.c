657ed8baa495ca79ad1c98f31492d124c22bc9fd
bhavik <bhaviksharma13dec@gmail.com>
2023-03-17 10:49:02 +0530

    Taking inputs

---
+++ calender.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int year,month,date;
+    printf("Enter Year - ");
+    scanf("%d",&year);
+    printf("Enter Month - ");
+    scanf("%d",&month);
+    printf("Enter Date - ");
+    scanf("%d",&date);
+}
