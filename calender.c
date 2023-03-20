6ae4a244fb4a9f2927f840f3b2426dd51b8ce278
chandresh1028 <122959352+chandresh1028@users.noreply.github.com>
2023-03-20 18:54:04 +0530

    Adding years

--- calender.c
+++ calender.c
@@ -7,4 +7,19 @@ int main(){
     scanf("%d",&month);
     printf("Enter Date - ");
     scanf("%d",&date);
+    char week_days[7][10]={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+    int y,s=0,day,flag=0;
+    int m;
+    if(year>=1900 && year<1906){
+        m=1900;
+    }
+    else if(year>=1906 && year<1917){
+        m=1906;
+    }
+    else if(year>=1917 && year<1923){
+        m=1917;
+    }
+    else if(year>=1923 && year<1934){
+        m=1923;
+    }
 }
